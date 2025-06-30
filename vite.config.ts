bacda9c26152b0b17bcf6dfc360a74fd57cd78ec
HunarSekhon <sekhonhunar95@gmail.com>
2025-06-30 09:19:12 -0700

    fetching fruits error fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_FRUITE_BASE_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy) => {
+          proxy.on('proxyReq', (proxyReq) => {
+            proxyReq.setHeader('x-api-key', process.env.VITE_FRUIT_API_KEY!);
+          });
+        }
+      }
+    }
+  }
 })
